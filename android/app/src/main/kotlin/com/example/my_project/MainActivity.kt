17b14a96be2c9c88ab7a221c48c2fee35ad20562
Ayush Shekhar <ayush221b@gmail.com>
2024-02-08 12:58:00 +0530

    Added platform specific code for method channels to show toast on Android

--- android/app/src/main/kotlin/com/example/my_project/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/my_project/MainActivity.kt
@@ -1,6 +1,30 @@
 package com.mycompany.methodchannelproject
 
+import android.widget.Toast
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterActivity() {
+
+    private val CHANNEL = "solutions.flutterflow.io/toast"
+
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
+                call, result ->
+            if (call.method == "showToast") {
+                val message = call.arguments<String>()
+                showToast(message)
+                result.success("Toast displayed in Android")
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
+
+    private fun showToast(message: String?) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
 }
